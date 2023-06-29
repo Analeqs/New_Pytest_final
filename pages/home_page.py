d122974ab6eacec94763db6196bd068b4525fd7f
unknown <anahitaleqs89@gmail.com>
2023-06-28 23:22:00 -0700

    (n)

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,3 +1,4 @@
+from pages import base_page
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
@@ -9,6 +10,12 @@ class HomePage(BasePage):
         super().__init__(driver)
 
     BMW_OPTION = '[id="bmwradio"]'
+    BENZ_OPTION = '[id="benzradio"]'
+    HONDA_OPTION = '[id="hondaradio"]'
 
     def check_bmw_option_is_selected(self):
         assert not self.check_element_selected((By.CSS_SELECTOR, self.BMW_OPTION))
+    def check_benz_option_is_selected(self):
+        assert self.check_element_selected((By.CSS_SELECTOR, self.BENZ_OPTION))
+    def check_honda_option_is_not_selected(self):
+        assert not self.check_element_selected((By.CSS_SELECTOR, self.HONDA_OPTION))
